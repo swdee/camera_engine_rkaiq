2d95a7d5e6098d997ccf7290f7e96f150db65ed5
swdee <shanon@bokumail.com>
2026-09-07 12:12:15 +1200

    Fix IQ parser header stripping on modern toolchains
    - Strip __fsid_t definitions from preprocessed headers
    - Preserve removal of host /usr include content
    - Keep generated parser headers compatible with newer Debian/Ubuntu toolchains

--- rkaiq/tools/iq_check/script/header_strip.sh
+++ rkaiq/tools/iq_check/script/header_strip.sh
@@ -3,5 +3,9 @@
 INPUT=$1
 OUTPUT=$2
 
-# Drop all global headers
-cat $INPUT |tac |sed '/^#.*"\/usr\//,$d' |sed '/__fsid_t/d' |tac |grep -v "^#" | sed -e 's/_Bool/_Bool\n/' | sed -e 's/\r//g'  > $OUTPUT
+cat "$INPUT" \
+    | sed '/__fsid_t/d' \
+    | grep -v "^#" \
+    | sed -e 's/_Bool/_Bool\n/' \
+    | sed -e 's/\r//g' \
+    > "$OUTPUT"
